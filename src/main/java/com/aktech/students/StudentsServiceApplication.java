30ed6c8f5fecfc98d0cbac12c630dafbb5807ed7
Anil Kesti (C) <anilkesti@AnilKestis-MacBook-Pro.local>
2022-06-24 10:50:02 +0400

    Getting service url from eureka with service name

--- src/main/java/com/aktech/students/StudentsServiceApplication.java
+++ src/main/java/com/aktech/students/StudentsServiceApplication.java
@@ -3,6 +3,7 @@ package com.aktech.students;
 import com.aktech.students.controller.StudentController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +21,7 @@ public class StudentsServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
